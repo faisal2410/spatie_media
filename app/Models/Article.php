267a5e1885fac823661704dde4b14f68f5c17b30
Faisal ahmed <faisal2410@yahoo.com>
2023-08-07 18:02:43 +0600

    updated

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 use Spatie\MediaLibrary\HasMedia;
+use Illuminate\Support\Facades\File;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\MediaLibrary\InteractsWithMedia;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class Article extends Model implements HasMedia
 {
@@ -14,5 +16,30 @@ class Article extends Model implements HasMedia
      public static function last()
     {
         return static::all()->last();
+    }
+
+
+      public function registerMediaConversions(Media $media = null): void
+    {
+        $this
+            ->addMediaConversion('thumb')
+            ->width(300)
+            ->height(300);
+    }
+
+
+       public function registerMediaCollections(): void
+    {
+        $this
+            ->addMediaCollection('big-files');
+
+            // ->acceptsFile(function(File $file){
+            //     return $file->mimeType==='image/jpeg';
+
+            // });
+
+            // ->singleFile()
+
+            //  ->useDisk('s3');  //For s3
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Models\User;
 use App\Models\Article;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArticleController;
+use Spatie\MediaLibrary\Support\MediaStream;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 /*
 |--------------------------------------------------------------------------
@@ -111,3 +114,76 @@ Route::get( 'send-a-file-to-s3', function () {
         ->toMediaCollection( 'images', 's3' );
 } );
 
+
+
+// downloading files
+
+Route::get( 'downloading-a-file', function () {
+    $media = Article::create()
+        ->addMedia( storage_path( 'demo/test.txt' ) )
+        ->toMediaCollection();
+
+    return $media;
+} );
+
+
+
+// Download file from s3
+
+Route::get( 'downloading-a-file-from-s3', function () {
+    $media = Article::create()
+        ->addMedia( storage_path( 'demo/test.txt' ) )
+        ->toMediaCollection( 'downloads', 's3' );
+
+    return $media;
+} );
+
+
+
+// Download Multiple files
+
+Route::get( 'downloading-multiple-files', function () {
+    Media::truncate();
+
+    Article::create()
+        ->addMedia( storage_path( 'demo/test1.txt' ) )
+        ->toMediaCollection();
+
+    Article::create()
+        ->addMedia( storage_path( 'demo/test2.txt' ) )
+        ->toMediaCollection( 'downloads');
+
+    return MediaStream::create( 'your-files.zip' )->addMedia( Media::all() );
+} );
+
+
+// Defining Media Collections
+
+Route::get( 'defining-media-collections', function () {
+    Article::create()
+        ->addMedia( storage_path( 'demo/test3.txt' ) )
+        ->toMediaCollection( 'big-files' );
+} );
+
+
+// Single File Collections
+
+Route::get( 'add-avatar', function () {
+    User::firstWhere( 'email', 'yparker@example.com' )
+        ->addMedia( storage_path( 'demo/avatar1.txt' ) )
+        ->toMediaCollection( 'avatar' );
+} );
+
+Route::get( 'update-avatar', function () {
+    User::firstWhere( 'email', 'yparker@example.com' )
+        ->addMedia( storage_path( 'demo/avatar2.txt' ) )
+        ->toMediaCollection( 'avatar' );
+} );
+
+//  Media Conversion
+
+Route::get('media-conversions', function () {
+    Article::create()
+        ->addMedia(storage_path('demo/library-dublin.jpg'))
+        ->toMediaCollection('images');
+});
