b406f0778e639879bb6fce340e3ba55d5472bfd9
Faisal ahmed <faisal2410@yahoo.com>
2023-08-07 16:05:26 +0600

    updated

--- routes/web.php
+++ routes/web.php
@@ -43,3 +43,71 @@ Route::get( 'add-multiple-files-media-to-library', function () {
 Route::get( 'add-media-from-request', [ArticleController::class, 'create'] );
 Route::post( 'add-media-from-request', [ArticleController::class, 'store'] );
 
+
+Route::get( 'not-preserving-original', function () {
+    Article::create()
+        ->addMedia( storage_path( 'demo/php1.jpeg' ) )
+        ->toMediaCollection();
+} );
+
+Route::get( 'preserving-original', function () {
+    Article::create()
+        ->addMedia( storage_path( 'demo/python.jpeg' ) )
+        ->preservingOriginal()
+        ->toMediaCollection();
+} );
+
+
+Route::get( 'other-options', function () {
+    Article::create()
+        ->addMedia( storage_path( 'demo/react.jpeg' ) )
+        ->usingName( 'A beautiful react image' )
+        ->usingFileName( 'other-name.jpg' )
+        ->withCustomProperties( [
+            'location' => 'Bangladesh, Sylhet',
+            'subject'  => 'Library',
+        ] )
+        ->toMediaCollection();
+} );
+
+// For deleting
+
+Route::get('step-1-add-media-to-models', function () {
+    Article::create()
+        ->addMedia(storage_path('demo/python.jpeg'))
+        ->toMediaCollection();
+});
+
+Route::get('step-2-delete-models', function () {
+    Article::all()->each->delete();
+});
+
+
+// using collections
+
+Route::get( 'using-collections', function () {
+
+    $article = Article::create();
+
+    $article
+        ->addMedia( storage_path( 'demo/php.jpeg' ) )
+        ->toMediaLibrary('images');
+
+    $article
+        ->addMedia( storage_path( 'demo/python.jpeg' ) )
+        ->toMediaCollection( 'images' );
+
+    $article
+        ->addMedia( storage_path( 'demo/react.jpeg' ) )
+        ->toMediaCollection( 'downloads' );
+} );
+
+
+
+// To store in s3 (we need aws s3 credentials)
+Route::get( 'send-a-file-to-s3', function () {
+    Article::create()
+        ->addMedia( storage_path( 'demo/react.jpeg' ) )
+        ->toMediaCollection( 'images', 's3' );
+} );
+
