a740dabf2c475ff6a62d9fb00e09e521892cef63
Faisal ahmed <faisal2410@yahoo.com>
2023-08-07 21:46:01 +0600

    updated

--- note.js
+++ note.js
@@ -1,3 +1,15 @@
+
+/*
+✅👇Study Reference :
+https://github.com/spatie/medialibrary-demo
+https://github.com/faisal2410/spatie_media
+https://spatie.be/courses/discovering-laravel-media-library/introducing-laravel-media-library
+
+https://spatie.be/docs/laravel-medialibrary/v10/introduction
+
+
+*/
+
 /*
 installation :
 composer require "spatie/laravel-medialibrary:^10.0.0"
